eaf8ffde50d7835715b35110ef146d2fe5c4ce48
Francisco Claudino <franciscoclaudino@Francisco-MacBook-Pro.local>
2024-06-09 22:15:10 +0100

    Update Neo4J Exploration

--- neo4j/queries_gerais.sql
+++ neo4j/queries_gerais.sql
@@ -20,40 +20,75 @@ RETURN h
 
 -- 5)
 -- Listar todos os episódios médicos de um paciente específico.
-
+MATCH (p:Patient {id_patient: 1})-[:HAS_EPISODE]->(e:Episode)
+RETURN e
 
 -- 6)
 -- Listar episódios médicos por tipo de condição.
+MATCH (p:Patient)-[:HAS_MEDICAL_HISTORY]->(mh:MedicalHistory {condition: 'Diabetes'})
+MATCH (p)-[:HAS_EPISODE]->(e:Episode)
+RETURN e
 
 -- 7)
 -- Listar todos os episódios médicos tratados por um médico específico.
+MATCH (d:Staff {id_emp: 1})<-[:CONDUCTED_BY]-(a:Appointment)<-[:HAS_APPOINTMENT]-(e:Episode)
+RETURN e
 
 -- 8)
 -- Listar todos os exames laboratoriais para um paciente específico.
+MATCH (p:Patient {id_patient: 1})-[:HAS_EPISODE]->(e:Episode)-[:HAS_LAB_SCREENING]->(ls:LabScreening)
+RETURN ls
 
 -- 9)
 -- Listar exames baseados no técnico responsável.
+MATCH (t:Staff {id_emp: 46})<-[:PERFORMED_BY]-(ls:LabScreening)
+RETURN ls
 
 -- 10)
--- Listar todas as faturas emitidas por um médico específico.
+-- Listar todas as faturas para um paciente específico.
+MATCH (p:Patient {id_patient: 2})-[:HAS_EPISODE]->(e:Episode)-[:HAS_BILL]->(b:Bill)
+RETURN b
 
 -- 11)
 -- Listar todas as consultas agendadas para um paciente específico.
+MATCH (p:Patient {id_patient: 1})-[:HAS_EPISODE]->(e:Episode)-[:HAS_APPOINTMENT]->(a:Appointment)
+RETURN a
 
 -- 12)
 -- Listar consultas baseadas no médico responsável.
+MATCH (d:Staff {id_emp: 1})<-[:CONDUCTED_BY]-(a:Appointment)
+RETURN a
 
 -- 13)
 -- Listar os Appointment para um dado Medico (por dia)
+MATCH (d:Staff {id_emp: 1})<-[:CONDUCTED_BY]-(a:Appointment)
+WHERE date(a.appointment_date) = date('2023-11-29')
+RETURN a
 
 -- 14)
 -- Buscar Appointment por data
+MATCH (a:Appointment)
+WHERE date(a.appointment_date) = date('2023-11-29')
+RETURN a
 
 -- 15)
 -- Buscar Appointment por data e depois por hora
+MATCH (a:Appointment)
+WHERE date(a.appointment_date) = date('2023-11-29') AND a.appointment_time = '18:11'
+RETURN a
 
 -- 16)
 -- Lista todos os episódios e o respetivo paciente
+MATCH (p:Patient)-[:HAS_EPISODE]->(e:Episode)
+RETURN e, p
 
 -- 17)
 -- Lista os médicos com mais consultas marcadas, com informação detalhada do paciente
+MATCH (d:Staff)-[:CONDUCTED_BY]-(a:Appointment)
+WITH d, count(a) AS appointment_count
+ORDER BY appointment_count DESC
+
+MATCH (d:Staff)-[:CONDUCTED_BY]-(a:Appointment)<-[:HAS_APPOINTMENT]-(e:Episode)-[:HAS_EPISODE]-(p:Patient)
+RETURN d.id_emp AS doctor_id, d.emp_fname AS doctor_first_name, d.emp_lname AS doctor_last_name, 
+       d.email AS doctor_email, appointment_count, collect(p) AS patients
+ORDER BY appointment_count DESC
